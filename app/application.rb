c934fec819d7de3fae16c7fd41892a29015c335f
Kirby Campbell <jkirbycampbell@gmail.com>
2018-07-11 21:24:49 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -16,7 +16,8 @@ class Application
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       @@cart.each do |item|
-      resp.write "#{item}\n"
+        resp.write "#{item}\n"
+      end
     else
       resp.write "Path Not Found"
     end
